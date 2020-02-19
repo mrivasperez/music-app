c696e98adad49933bda856d04545c2dac651f306
mrivasperez <mrivasperez1996@gmail.com>
2020-02-18 20:29:36 -0800

    began script

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Misc Info
+const musicContainer = document.getElementById('music-container');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+// Buttons
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+// Music
+const audio = document.getElementById('audio');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
