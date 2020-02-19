3443f66ed7b5ae5ababeb90d98e5771122944e16
mrivasperez <mrivasperez1996@gmail.com>
2020-02-18 20:49:20 -0800

    changed track and cover sources

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Making all constants
 // Misc Info
 const musicContainer = document.getElementById('music-container');
 const progressContainer = document.getElementById('progress-container');
@@ -10,3 +11,17 @@ const nextBtn = document.getElementById('next');
 const audio = document.getElementById('audio');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+
+// Array of song titles
+const songs = ['creativeminds', 'goinghigher', 'tomorrow'];
+// Keep track of song
+let songIndex = 1;
+//Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song){
+    title.innerText = song;
+    audio.src = `assets/sounds/${song}.mp3`;
+    cover.src = `assets/images/${song}.jpg`;
+}
